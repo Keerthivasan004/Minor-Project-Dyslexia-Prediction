2b8067586acce980b77096582a9e8a7e569ec5fc
Keerthivasan004 <keerthivasanVA@gmail.com>
2024-11-14 19:22:58 +0530

    header

--- project-code/simple.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-app=Flask(__name__)
-@app.route('/')
-def hello():
-    return "Hello World"
-@app.route('/user/<users>')
-def user(users):
-    return f'<h1>Hello from {users}</h1>'
