a45b88212c12f1ec0bf3356936e182a8a49de082
Keerthivasan004 <keerthivasanVA@gmail.com>
2024-11-19 20:05:04 +0530

    intergrating javascript for navigation on process

--- project-code/flask1.py
+++ project-code/flask1.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app=Flask(__name__)
 @app.route('/')
-@app.route('/prediction-page-1')
+@app.route('/prediction_page_1')
 def prediction_page_1():
     return render_template('webpage1.html')
+@app.route('/registration-page')
+def registration_page():
+    return redirect(url_for('webpage3.html'))
