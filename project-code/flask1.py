99149fe07562db11d07e59bf6909fb230adce8db
Keerthivasan004 <keerthivasanVA@gmail.com>
2024-11-20 13:45:31 +0530

    fixed the webpage 3 connection by using forms for label

--- project-code/flask1.py
+++ project-code/flask1.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 app=Flask(__name__)
 @app.route('/')
 @app.route('/prediction_page_1')
 def prediction_page_1():
     return render_template('webpage1.html')
-@app.route('/registration-page')
-def registration_page():
-    return redirect(url_for('webpage3.html'))
+@app.route('/prediction_page_2')
+def prediction_page_2():
+    return render_template('webpage3.html')
